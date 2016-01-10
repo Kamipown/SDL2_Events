9934c6d428aced3b7be2cdd8888d95b26c21da89
Paul Delobbel <paul.delobbel@gmail.com>
2016-01-11 00:20:06 +0100

    main simplified - new_window() changed

--- main.c
+++ main.c
@@ -18,11 +18,18 @@ static void			initialization(Uint32 flags)
 	}
 }
 
-static SDL_Window	*new_window(const char *title, int x, int y, Uint32 flags)
+static SDL_Window	*new_window(int x, int y, int argc, char *argv[])
 {
 	SDL_Window	*window;
 
-	if ((window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, x, y, flags)) == NULL)
+	if (argc == 3)
+		window = SDL_CreateWindow("Yo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, atoi(argv[1]), atoi(argv[2]), 0);
+	else
+	{
+		window = SDL_CreateWindow("Yo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, x, y, 0);
+		printf("You can use ./SDL_2_Windows 'width' 'height'\n");
+	}
+	if (window == NULL)
 	{
 		printf("SDL_CreateWindow failed: %s\n", SDL_GetError());
 		exit(EXIT_FAILURE);
@@ -46,13 +53,7 @@ int					main(int argc, char *argv[])
 	t_inputs	inputs;
 
 	initialization(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
-	if (argc == 3)
-		window = new_window("Yo", atoi(argv[1]), atoi(argv[2]), 0);
-	else
-	{
-		window = new_window("Yo", 800, 600, 0);
-		printf("You can use ./SDL_2_Windows 'width' 'height'\n");
-	}
+	window = new_window(800, 600, argc, argv);
 	inputs.run = 1;
 	while (inputs.run)
 	{
