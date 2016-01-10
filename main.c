6c6557107325693ae37f029dc88e98c9f84baf69
Paul Delobbel <paul.delobbel@gmail.com>
2016-01-10 23:37:39 +0100

    Window opened

--- main.c
+++ main.c
@@ -13,11 +13,32 @@ static void			initialization(Uint32 flags)
 	}
 }
 
+static SDL_Window	*new_window(const char *title, int x, int y, Uint32 flags)
+{
+	SDL_Window	*window;
+
+	if ((window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, x, y, flags)) == NULL)
+	{
+		printf("SDL_CreateWindow failed: %s\n", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+	return (window);
+}
+
 int					main(int argc, char *argv[])
 {
-	if (argc && argv)    // Just for
-		SDL_Delay(100);  // the flags.
-	initialization(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+	SDL_Window	*window;
+
+	initialization(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+	if (argc == 3)
+		window = new_window("Yo", atoi(argv[1]), atoi(argv[2]), 0);
+	else
+	{
+		window = new_window("Yo", 800, 600, 0);
+		printf("You can use ./SDL_2_Windows 'width' 'height'\n");
+	}
+	SDL_Delay(1000);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return (EXIT_SUCCESS);
 }
